c4e320a1f72bb313f07c8dbd15bdd0f67d03e2cb
Rleaf <calcifer44@gmail.com>
2022-12-19 21:02:03 -0500

    dinner was good :)

--- backend/routes/api/summoners.js
+++ backend/routes/api/summoners.js
@@ -26,10 +26,8 @@ router
          const result = (await client.collection(summoner.name).find({}).toArray()).shift()
 
          // Check if summoner is in database
-         if (result == undefined) {
-            console.log('Summoner not in database, adding...')
-            await summonerCheckInitialMatchPullv2(client, summoner, req.params.region)
-         }
+         
+         await summonerCheckInitialMatchPullv2(client, result, summoner, req.params.region)
 
          // UTIL 
          // await client.collection('fri').deleteMany()
@@ -59,8 +57,12 @@ async function loadSummonerCollection() {
    return client.db('summoners')
 }
 
-async function summonerCheckInitialMatchPullv2(client, summoner, region) {
+async function summonerCheckInitialMatchPullv2(client, result, summoner, region) {
    // Add new summoner to database
+   if (result != undefined) {
+      return
+   }
+
    await client.collection(summoner.name).insertOne(summoner)
    await client.collection(summoner.name).updateOne({'name': summoner.name}, {$set : {'parsedIndex': 0}})
    
@@ -76,83 +78,72 @@ async function summonerCheckInitialMatchPullv2(client, summoner, region) {
 
 async function matchParserV2(client, summoner, region) {
    const result = (await client.collection(summoner.name).find({}).toArray()).shift()
-   // Check if there are already parsed matches
 
    let parsedIndex = result.parsedIndex
    const matchLength = result.matchId.length
-   let matchContainer
    
-   // Check if existing match containers
-   // const huh = (await client.collection(summoner.name).find().sort({ matchEnd: -1 }).limit(1).toArray()).shift()
-   const huh = (await client.collection(summoner.name).findOne({ matchEnd: {$exists: true}}))
-
-   if (huh == undefined) {
-      // Summoner has no recorded parsed matches.
-      console.log(`No existing parsed matches`)
-      await client.collection(summoner.name).insertOne({matchStart: 0, matchEnd: 25, matches: []})
-      // matchContainer = (await client.collection(summoner.name).findOne({matchStart: 0})).matches
-      matchContainer = 25
-
-   } else {
-      // Summoner has existing parsed matches.
-      console.log(`Has existing parsed matches`)
-      matchContainer = (await client.collection(summoner.name).find().sort({ matchEnd: -1 }).limit(1).toArray()).shift().matchEnd
-   }
+   // Check if existing parses || init matchContainer
+   let matchEndIndex = await parseCheck(client, summoner)
+   
    
    console.log(`Starting parse at ${parsedIndex}`)
    while (parsedIndex <= matchLength) {
       
-      console.log(`Parsed ${parsedIndex} matches`)
-      // if (parsedIndex % 10 == 0) {
-      // }
+      console.log(`Parsed ${parsedIndex + 1} matches`)
       
       const match = await twisted.getMatchInfo(result.matchId[parsedIndex], region)
-         .catch((e) => {
-            if (parsedIndex != matchLength) {
-               parsedIndex--                
-               // await client.connection(summoner.name).deleteOne()
-            }
-            console.log(`Got rate limit check, reducing parsedIndex by 1`, e)
+         .catch(async (e) => {
+            console.log(e.status)
          })
-
-      await client.collection(summoner.name).updateOne({'matchEnd': matchContainer}, {$push: {'matches': match}})
       
-      if (parsedIndex != 0 && parsedIndex % 25 == 0) {
-         containerExists = await client.collection(summoner.name).findOne({ matchEnd: parsedIndex+25})
+      if (match != null) {
+         await client.collection(summoner.name).updateOne({'matchEnd': matchEndIndex}, {$push: {'matches': match}})
+         await client.collection(summoner.name).updateOne({'name': summoner.name}, {$set: {'parsedIndex': parsedIndex}})
 
-         if (containerExists == null) {
-            matchContainer = await addMatchContainer(client, summoner, parsedIndex, parsedIndex+25)
+         if (parsedIndex != 0 && parsedIndex % 25 == 0) {
+            matchEndIndex = await addMatchContainer(client, summoner, parsedIndex, parsedIndex+25)
          }
+
+         parsedIndex++
+         
+      } else {
+         console.log(`Got rate limit check, recycling same game.`)
       }
-      
+
       if (parsedIndex == matchLength) {
-         console.log(`Setting parse to ${parsedIndex}`)
-         await client.collection(summoner.name).updateOne({name: summoner.name}, {$set: {'parsedIndex': parsedIndex}})
          console.log(`I am done :)`)
          break
       }
-      
-      parsedIndex++
-   }
 
-   // console.log(`Starting parse at ${parsedIndex}`)
-   // for ( ; parsedIndex < matchLength; parsedIndex++) {
-   //    console.log(`carrot cake ${parsedIndex}`)
-   //    const match = await twisted.getMatchInfo(result.matchId[parsedIndex], region)
-   //       .catch((e) => {
-   //          console.log('yuh', e)
-   //       })
-   //    // console.log(matchContainer)
       
-   //    if (parsedIndex != 0 && parsedIndex % 25 == 0) {
-   //       matchContainer = await addMatchContainer(client, summoner, parsedIndex, parsedIndex+25)
-   //    }
-      
-   //    if (parsedIndex == matchLength - 1) {
-   //       await client.collection(summoner.name).updateOne({name: summoner.name}, {$set: {'parsedIndex': parsedIndex + 1}})
-   //    }
-   //    await client.collection(summoner.name).updateOne({'matchEnd': matchContainer}, {$push: {'matches': match}})
-   // }
+   }
+}
+
+async function removeParse(client, summoner, matchEndIndex) {
+   const toad2 = (await client.collection(summoner.name).find({'matchEnd': matchEndIndex})).toArray()
+   console.log('toad', await toad2)
+
+   await client.collection(summoner.name).updateOne({'matchEnd': matchEndIndex}, {$pop: {matches: 1}})
+
+   const toad = (await client.collection(summoner.name).find({'matchEnd': matchEndIndex})).toArray()
+   console.log('toad', await toad)
+   console.log(`Got rate limit check, reducing parsedIndex by 1`)
+}
+
+async function parseCheck(client, summoner) {
+
+   const toad = (await client.collection(summoner.name).findOne({ matchEnd: {$exists: true}}))
+   let matchContainer
+
+   if (toad == undefined) {
+      // Summoner has no recorded parsed matches.
+      console.log(`No existing parsed matches`)
+      return matchContainer = addMatchContainer(client, summoner, 0, 25)
+   } else {
+      // Summoner has existing parsed matches.
+      console.log(`Has existing parsed matches`)
+      return matchContainer = (await client.collection(summoner.name).find().sort({ matchEnd: -1 }).limit(1).toArray()).shift().matchEnd
+   }
 }
 
 async function addMatchContainer(client, summoner, start, end) {
