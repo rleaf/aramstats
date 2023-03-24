27919f776d636d25995f8a3170be013a5b0eb476
Rleaf <calcifer44@gmail.com>
2023-03-23 23:16:03 -0400

    + milio

--- src/assets/aram_champ_classes.js
+++ src/assets/aram_champ_classes.js
@@ -98,6 +98,7 @@ export const classBook = {
    malzahar: ['Battlemage'],
    maokai: ['Vanguard'],
    masteryi: ['Skirmisher', 'Assassin'], /* +Assassin */
+   milio: ['enchanter'],
    missfortune: ['Artillery', 'Marksman'], /* +Artillery */
    monkeyking: ['Diver'],
    mordekaiser: ['Juggernaut'],
