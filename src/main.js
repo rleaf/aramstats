9dfb190ef841d644eac7f78d33e552b18b303f56
Rleaf <calcifer44@gmail.com>
2023-04-20 16:01:00 -0400

    analytics

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueGtag from 'vue-gtag'
 
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(VueGtag, {
+   config: { id: "G-PD6QYJ923C"}
+})
 
 app.mount('#app')
