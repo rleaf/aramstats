37a3c13a9d60cc650e0f0282e5a235a54b68428f
Ryan <calcifer44@gmail.com>
2024-03-30 19:49:27 -0400

    work on teamfight classifier

--- backend/routes/summonerNew.js
+++ backend/routes/summonerNew.js
@@ -49,6 +49,194 @@ function getPlayerUsefulness(victims, playerIndex) {
    return [use, exp]
 }
 
+function parseTimeline3(timeline, playerIndex) {
+   const CONTIGUITY = 5000
+   const AVG_TEAMFIGHT_DISTANCE = 1300
+   const BUILDING_KILL_WINDOW = 30000
+   let timelineData = {
+      exp: 0,   // expectation
+      cap: 0,   // capitalization
+      use: 0,   // usefullness
+      part: 0,  // participation
+      freq: 0,  // frequency
+      fs: 0,    // fountain sitting
+   }
+   let teamfights = []
+   let bin = []
+   let c1, c2
+   let initTimestamp
+   let capFlag
+   let tfPrerequisite = 0
+
+   //  ITER FRAMES
+   for (let i = 0; i < timeline.info.frames.length; i++) {
+      let e = timeline.info.frames[i].events
+
+      // ITER EVENTS
+      for (let j = 0; j < e.length; j++) {
+         if (e[j].timestamp - capFlag <= BUILDING_KILL_WINDOW && e[j].type === 'BUILDING_KILL') {
+            timelineData.cap++
+            capFlag = undefined
+         }
+
+         if (e[j].timestamp - initTimestamp > CONTIGUITY) {
+            if (tfPrerequisite > 1 && bin.length > 3 && averageDistance(bin) < AVG_TEAMFIGHT_DISTANCE) {
+               teamfights.push(bin)
+               capFlag = e[j].timestamp
+            }
+
+            tfPrerequisite = 0
+            initTimestamp = c1 = c2 = undefined
+            bin = []
+         }
+
+         if (e[j].type === 'CHAMPION_KILL') {
+            if (e[j].timestamp - initTimestamp <= CONTIGUITY) {
+               if ('assistingParticipantIds' in e[j] && e[j].assistingParticipantIds.length >= 2) tfPrerequisite++
+               initTimestamp = e[j].timestamp
+               bin.push(e[j])
+            } else {
+               if ('assistingParticipantIds' in e[j] && e[j].assistingParticipantIds.length >= 2) tfPrerequisite++
+               
+               if (!initTimestamp) {
+                  initTimestamp = e[j].timestamp
+                  bin.push(e[j])
+               }
+            }
+         }
+      }
+   }
+
+   // Frequency
+   timelineData.freq = teamfights.length
+
+   for (let i = 0; i < teamfights.length; i++) {
+      let use = []
+      let part = false
+      for (let j = 0; j < teamfights[i].length; j++) {
+         const cell = teamfights[i][j]
+
+         // Expectation
+         if ((playerIndex <= 5 && cell.killerId <= 5) || (playerIndex > 5 && cell.killerId > 5)) timelineData.exp++
+         if ((playerIndex <= 5 && cell.victimId <= 5) || (playerIndex > 5 && cell.victimId > 5)) timelineData.exp--
+         
+         // Participation
+         if (cell.assistingParticipantIds[playerIndex] || cell.killerId === playerIndex) part = true
+
+         // Usefullness
+         if ((playerIndex <= 5 && cell.victimId <= 5) || (playerIndex > 5 && cell.victimId > 5)) use.push(cell.victimId)
+      }
+      if (part) timelineData.part++
+      
+      // Usefullness
+      timelineData.use += use.findIndex(o => o === playerIndex) + 1
+   }
+
+   const af = (o) => {
+      return Math.round((o / timelineData.freq) * 10) / 10
+   }
+
+   timelineData.exp = af(timelineData.exp)
+   timelineData.use = af(timelineData.use)
+
+   return timelineData
+}
+
+function averageDistance(bin) {
+   bin = bin.map(x => ({x: x.position.x, y: x.position.y}))
+
+   let arr = []
+   let avg
+   for (let i = 0; i < bin.length; i++) {
+      for (let j = i + 1; j < bin.length; j++) {
+         arr.push(Math.sqrt(Math.pow(bin[i].x - bin[j].x, 2) + Math.pow(bin[i].y - bin[j].y, 2)))
+      }
+   }
+   avg = arr.reduce((a, b) => a + b, 0) / arr.length
+   return avg
+}
+
+function parseTimeline2(timeline, playerIndex) {
+   // Use a "middle-out" approach that will iterate, more efficiently, than parseTimeline. Instead of checking every single CHAMPION_KILL and it's proceeding ~30 seconds of data to see if a teamfight exists, it only looks at every CHAMPION_KILL with 3+ assists. It then looks at the proceeding 8 seconds to see if there's another CHAMPION_KILL with 3+ assists in favor of the other team. If both of these CHAMPION_KILLS flag true, then it's a teamfight.
+
+   // Although it's more efficient, it's a little more painful grabbing all of the pertaining data because, now, instead of only having to look forward for relevant data, I also need to go backwards since the flag for a teamfight is likely to occur in the middle of a teamfight.
+
+   // UPDATE: Okay Jared reminded me that this wouldn't work for 5-0 teamfights, because one side won't die at all. All my homies hate Jared. Instead, we discussed qualifying teamfights based off CHAMPION_KILL death proximities. If they're close enough, then it's a teamfight. https://discord.com/channels/@me/230170098785189889/1223021090998063195
+
+   let timelineData = {
+      exp: 0,   // expectation
+      cap: 0,   // capitalization
+      use: 0,   // usefullness
+      part: 0,  // participation
+      freq: 0,  // frequency
+      fs: 0,    // fountain sitting @@@
+   }
+   let teamfights = []
+   let t1 // timestamp_1
+   const teamCheck = (id) => (id > 5) ? 0 : 1
+
+
+   // ITER FRAMES
+   for (let i = 0; i < timeline.info.frames.length; i++) {
+      const e = timeline.info.frames[i].events
+      // const e2 = timeline.info.frames[i+1].events
+      const e2 = (timeline.info.frames[i+1]) ? timeline.info.frames[i+1].events : null
+      let demarcate = false
+      let f1, f2 // flags
+      let c1, c2 // team check
+      
+      // ITER EVENTS
+      for (let j = 0; j < e.length; j++) {
+         if (('assistingParticipantIds' in e[j] && e[j].assistingParticipantIds.length > 3)) {
+            if (!f1) { // Initial flag check
+               c1 = teamCheck(e[j].killerId)
+               t1 = e[j].timestamp
+               f1 = j
+               continue
+            } else {
+               if (e[j].timestamp - t1 <= 8000) {
+                  c2 = teamCheck(e[j].killerId)
+                  if (c1 !== c2) {
+                     f2 = j
+                     break
+                  }
+
+                  if (e2 && !f2 && !e[j+1]) { // In the event a demarcation is between a teamfight.
+                     for (let k = 0; k < e2.length; k++) {
+                        if (e2[k].timestamp - t1 <= 8000) break
+                        if ('assistingParticipantIds' in e2[k] && e2[k].assistingParticipantIds.length > 3) {
+                           c2 = teamCheck(e2[k].killerId)
+                           if (c1 !== c2) {
+                              demarcate = true // Detect if the index is on the proceeding frame
+                              f2 = k
+                              break
+                           }
+                        }
+                     }
+                  }
+               } else {
+                  f1 = f2 = null
+               }
+            }
+         }
+      }
+
+      // if (f1 && f2) teamfights.push([i, f1, f2])
+      console.log(f1, f2)
+      if (typeof f1 === 'number' && typeof f2 === 'number') teamfights.push([i, f1, f2])
+   }
+   console.log(teamfights, 'tf')
+   for (let i in teamfights) {
+      const tf = teamfights[i]
+      const tl = timeline.info.frames[tf[0]]
+
+      // 
+      for (let j = 0; j < tl[2]; j++) {
+
+      }
+   }
+}
+
 function parseTimeline(timeline, playerIndex) {
    // Timeline explanations: https://www.figma.com/file/iOKkrg5bBTd5IFp3ghjSS0/Aramstats?type=whiteboard&node-id=0%3A1&t=YEwCSx4b514A9eFe-1
    let capFlag = false
@@ -60,79 +248,96 @@ function parseTimeline(timeline, playerIndex) {
       freq: 0,  // frequency
    }
    
-   for (let i = 1; i < timeline.info.frames.length - 1; i++) {
+   //  ITER FRAMES
+   for (let i = 0; i < timeline.info.frames.length; i++) {
       let e = timeline.info.frames[i].events
       let victims = []
       let participants = new Set()
-      let tf_1
-      let k1
       let tf = false
       let tfContainer = []
       
+      // ITER EVENTS
       for (let j = 0; j < e.length; j++) {
+         /**
+          * Prerequisites for a teamfight are any champion kill.
+          * After every champion kill, look into the proceeding 30 seconds to find any teamfighting corroborating data.
+          * Corroborating data is any champion kill accompanied with 3 assists. For a teamfight to be flagged, both sides must have corroborating data.
+         */
+
          if (capFlag && e[j].type === 'BUILDING_KILL') {
             timelineData.cap++
             capFlag = false
          }
          
-         // if (e[j].type === 'CHAMPION_KILL') {
-         //    tf_1 = e[j].timestamp
-         //    k1 = e[j].killerId
-         //    for (let o = j + 1; o < e.length; o++) {
-         //       if ((e[j].timestamp - e[o].timestamp) >= 15000) break
-         //       if (e[o].type !== 'CHAMPION_KILL') continue
-         //       tfContainer.push(e[o])
-         //    }
+         if (e[j].type === 'CHAMPION_KILL') {
+            let o = j + 1
+            let flag = false
+            for (o; o < e.length; o++) {
+               // if (!e[o]) {
+               //    e = timeline.info.frames[i + 1].events
+               //    o = 0
+               //    flag = true
+               // }
+               if ((e[o].timestamp - e[j].timestamp) >= 30000) break
+               if (e[o].type === 'CHAMPION_KILL') tfContainer.push(e[o])
+            }
+            j += o
+            if (flag) e = timeline.info.frames[i].events
+         }
+         // console.log(e[j-1], 'turkeys')
 
-         //    if (!('assistingParticipantIds' in e[j]) || e[j].assistingParticipantIds.length < 3) continue
+         if (tfContainer) {
+            let f1, f2
+            for (let k = 0; k < tfContainer.length; k++) {
+               // if ('assistingParticipantIds' in tfContainer[k]) {
+               //    tfContainer[k].assistingParticipantIds.forEach(p => participants.add(p))
+               //    if (tfContainer[k].assistingParticipantIds.length < 3) continue
+               // }
+               victims.push(tfContainer[k].victimId)
+               if (!('assistingParticipantIds' in tfContainer[k]) || tfContainer[k].assistingParticipantIds.length < 3) continue
+               tfContainer[k].assistingParticipantIds.forEach(p => participants.add(p))
+               if (tfContainer[k].killerId <= 5) {
+                  f1 = true
+               } else {
+                  f2 = true
+               }
+               if (f1 && f2) {
+                  console.log(`teamfight detected on frame ${i}, timestamp ${tfContainer[k].timestamp}`)
+                  tf = true
+               }
+            }
+         }
 
-         //    e[j].assistingParticipantIds.forEach(p => participants.add(p))
-         //    victims.push(e[j].victimId)
+         tfContainer = []
 
+         if (tf) {
+            let t2 = 0
+            let e2 = 0
+            let use = []
+            capFlag = true
 
-         //    for (let k = 0; k < tfContainer.length; k++) {
-         //       victims.push(e[k].victimId)
-         //       e[k].assistingParticipantIds.forEach(p => participants.add(p))
-         //       if (!('assistingParticipantIds' in e[k]) || e[k].assistingParticipantIds.length < 3) continue
-         //       if ((e[k].killerId <= 5 && k1 <= 5) || (e[k].killerId > 5 && k1 > 5)) continue
-         //       tf = true
+            timelineData.freq++
 
-         //       if (k === tfContainer.length - 1) j += k
-         //    }
+            victims.forEach(v => {
+               if (v <= 5 && playerIndex <= 5) {
+                  use.push(v)
+                  t2++
+               } else {
+                  e2++
+               }
 
-         //    if (tf) {
-         //       console.log(`teamfight detected on frame ${i}, timestamp ${tf_1}`)
-         //       let t = 0
-         //       let e = 0
-         //       let f = []
-
-         //       timelineData.freq++
-
-         //       victims.forEach(v => {
-         //          if (v <= 5 && playerIndex <= 5) {
-         //             f.push(v)
-         //             t++
-         //          } else {
-         //             e++
-         //          }
-
-         //          if (v > 5 && playerIndex > 5) {
-         //             t++
-         //          } else {
-         //             e++
-         //          }
-         //       })
-               
-         //       timelineData.use += f.findIndex(o => o === playerIndex) + 1
-         //       timelineData.exp += (t - e)               
-         //       if (participants.has(playerIndex)) timelineData.part++
-         //       if (!capFlag) capFlag = false
-         //       // t1p = null
-         //       // t2p = null
-         //       tf = false
-         //    }
-   
-         // }
+               if (v > 5 && playerIndex > 5) {
+                  t2++
+               } else {
+                  e2++
+               }
+            })
+            
+            timelineData.use += use.findIndex(o => o === playerIndex) + 1
+            timelineData.exp += (e2 - t2)               
+            if (participants.has(playerIndex)) timelineData.part++
+            tf = false
+         }
       }
    }
 
@@ -182,10 +387,10 @@ async function parseMatchlist(summonerDocument, matchlist, update=false) {
       } catch(e) {}
 
       if (timeline) {
-         timelineData = parseTimeline(timeline, playerIndex)
+         timelineData = parseTimeline3(timeline, playerIndex)
          console.log(timelineData, 'toadies')
       }
-      // console.log(turkeys)
+      console.log(turkeys)
 
       const matchDocument = new summonerMatchesModel({
          m: summonerDocument._id,
@@ -298,13 +503,13 @@ async function parseMatchlist(summonerDocument, matchlist, update=false) {
          }
       }
 
-      await puuidModel.bulkWrite(participantPuuids)
-         .catch(e => {
-            throw e
-         })
+      // await puuidModel.bulkWrite(participantPuuids)
+      //    .catch(e => {
+      //       throw e
+      //    })
 
-      await matchDocument.save()
-      await summonerDocument.save()
+      // await matchDocument.save()
+      // await summonerDocument.save()
    }
 }
 
--- backend/routes/testo.js
+++ backend/routes/testo.js
@@ -1,6 +1,54 @@
-const arr = [1, 2, 3, 4, 5]
-let v = 5
-let h
-let g = Math.abs(v - h)
-console.log((g < 5))
-if (g < 5) console.log('toad')
+const arr = [1, 2, 3, 4, 5, 6]
+const a = {a: 1, b: 2, c: 3}
+const b = {a: 1, b: 2, d: 3}
+let c
+let d = 5
+console.log(c + d)
+if (c+d) console.log('toad')
+let f1
+if (10 - c > 5) console.log('rhino');
+if (!f1) console.log('turkey');
+console.log(Math.pow(-2, 2))
+// ((x) => {
+//    console.log(x)
+// })('toads')
+
+delete arr[3]
+arr
+
+let arr2 = [
+   {
+      assistingParticipantIds: [Array],
+      bounty: 180,
+      killStreakLength: 1,
+      killerId: 10,
+      position: {
+         "x": 123,
+         "y": 321
+      }
+   },
+   {
+      assistingParticipantIds: [Array],
+      bounty: 180,
+      killStreakLength: 0,
+      killerId: 8,
+      position: {
+         "x": 234,
+         "y": 432
+      }
+   },
+   {
+      assistingParticipantIds: [Array],
+      bounty: 180,
+      killStreakLength: 0,
+      killerId: 4,
+      position: {
+         "x": 345,
+         "y": 543
+      }
+   }
+]
+
+arr2 = arr2.map(x => ({ x: x.position.x, y: x.position.y }))
+// arr2 = arr2.flatMap((v, i) => arr2.slice(i+1).map(w => [v.position, w.position]))
+console.log(arr2)
