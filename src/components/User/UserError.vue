84085d4e1a5e6f776e4e38ddf69d64f29e30b82f
Ryan <calcifer44@gmail.com>
2024-08-30 02:23:00 -0400

    3.9.2

--- src/components/User/UserError.vue
+++ src/components/User/UserError.vue
@@ -4,6 +4,7 @@ export default {
       return {
          rouletteArr: [
             'https://preview.redd.it/dhywo66hzm7d1.jpeg?auto=webp&s=283c651d3956bb6492688825a1c941c5b65acedd',
+            'https://preview.redd.it/jdskl1qgy46d1.jpeg?auto=webp&s=38a508dedcec217b3fabd248da0113e98999adb6',
             'https://i.redd.it/6p956lq5yiq81.jpg',
             'https://i.redd.it/k8obvr07gfb91.png',
             'https://i.redd.it/uek3ydziwvtb1.jpg',
